22170219962c6a497afe94af26bd18bc201ede65
quocviet2396 <nqv2396@gmail.com>
2023-10-04 19:50:04 +0700

    TEST

---
+++ WebApp/Repositories/IFacilitiesServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibraryModels;
+
+namespace WebApp.Repositories
+{
+	public interface IFacilitiesServices
+	{
+         Task<IEnumerable<Facilities>> GetFacilities();
+        Task<bool> addFacilities(Facilities newFacilities);
+        Task<bool> editFacilities(Facilities newFacilities);
+
+        Task<Facilities> GetFacilitiesById(int id);
+        Task<bool> deleteFacilities(int id);
+    }
+}
+
---
+++ WebApp/Repositories/IPriorityServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibraryModels;
+
+namespace WebApp.Repositories
+{
+	public interface IPriorityServices
+	{
+        Task<IEnumerable<Priority>> GetPriority();
+        Task<bool> addPriority(Priority newPriority);
+        Task<bool> editPriority(Priority newPriority);
+
+        Task<Priority> GetPriorityById(int id);
+        Task<bool> deletePriority(int id);
+    }
+}
+
